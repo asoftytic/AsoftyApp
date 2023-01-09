2fe4ca1b0dcb748c5f5fa2d3e75bf114f0b2d91d
Miguel Alejandro Segura <segu803282@siigo.com>
2023-01-09 02:40:45 -0500

    Added Delete method to infrastructure CRUD (QueryGenericHandler class) and control for entity that is used in QueryGenericHandler<T> should inherit from DbEntity class

--- BackEnd/AsoftyBackend/AsoftyBackend.Infrastructure/ManualUnitTestComponent.cs
+++ BackEnd/AsoftyBackend/AsoftyBackend.Infrastructure/ManualUnitTestComponent.cs
@@ -13,19 +13,28 @@ namespace AsoftyBackend.Infrastructure
         public static async void Main()
         {
 
-            var cmd = new QueryGenericHandler<User>();
+            var db = new QueryGenericHandler<User>();
 
 
             //  Query Example
-            var user1 = await cmd.Where(u => u.Username == "UnitTest1" && u.Password == "12345678" && u.Enabled == true).QueryAsync();
+            var user1 = await db.Where(u => u.Username == "UnitTest1" && u.Password == "12345678" && u.Enabled == true).QueryAsync();
 
-            var usr = new User { CompanyCode = 0, Username = "George", Password = "Macaco12" };
+            //  Create random users
+            var usr = new User { CompanyCode = 0, Username = "George5", Password = "Macaco12" };
+            var usr1 = new User { CompanyCode = 0, Username = "George6", Password = "Macaco12" };
+            var usr2 = new User { CompanyCode = 0, Username = "George7", Password = "Macaco12" };
 
             //  Insert single
             //  Note: preferably insert objects of anonymous type, because if the type is used, you must declare all the necessary fields
-            await cmd.InsertAsync(usr);
+            var t0 = db.InsertAsync(usr);
+            var t1 = db.InsertAsync(usr1);
+            var t2 = db.InsertAsync(usr2);
 
-            
+            //  Delete example
+            var result = await db.DeleteWhere(u => u.Password == "Macaco12");   // Result needs to be 3
+
+            //  Delete one by PrimaryKey
+            result = await db.DeleteWhere(u => u.UserId == user1.First().UserId);
 
             ;
             
