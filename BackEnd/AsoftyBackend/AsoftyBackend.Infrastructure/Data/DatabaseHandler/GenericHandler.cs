5006ba4198b3590280a92a7ea59e91bbc32eca75
Miguel Alejandro Segura <segu803282@siigo.com>
2023-01-10 00:01:55 -0500

    Mejorado el sistema de consultas WHERE en infraestructura, utilizando clases anónimas y/o objetos en lugar de expresiones lambda

--- BackEnd/AsoftyBackend/AsoftyBackend.Infrastructure/Data/DatabaseHandler/GenericHandler.cs
+++ BackEnd/AsoftyBackend/AsoftyBackend.Infrastructure/Data/DatabaseHandler/GenericHandler.cs
@@ -31,14 +31,53 @@ public abstract class GenericHandler<T> where T : class, new()
 
     }
 
-    public virtual GenericHandler<T> Where(Expression<Func<T, bool>> expression)
+    //public virtual GenericHandler<T> Where(Expression<Func<T, bool>> expression)
+    //{
+    //    string sqlWhere = "";
+    //    ReflectionUtils.ExpressionToString<T>(expression, ref sqlWhere);
+
+    //    _where += $"{(_where.NullOrEmpty() ? "" : " AND ")}({sqlWhere})";
+
+    //    return this;
+    //}
+
+    protected virtual GenericHandler<T> Where(object entity, Operator op = Operator.Equals, LogicalNode AndNodeInternal = LogicalNode.True, LogicalNode AndNodeExternal = LogicalNode.True)
     {
-        string sqlWhere = "";
-        ReflectionUtils.ExpressionToString<T>(expression, ref sqlWhere);
 
-        _where += sqlWhere;
+        string sqlWhere = "(";
+        int i = 0;  //  Iterator
 
+        var properties = Utils.Utils.ObjectToDictionary(entity);
+
+        foreach(var property in properties)
+        {
+            if (property.Value == null) continue;
+
+            if (i > 0) sqlWhere += $" {(AndNodeInternal == LogicalNode.True ? "AND" : "OR")} ";
+
+            var propType = property.Value.GetType();
+            var val = property.Value.AntiInjectionFormat();
+
+            sqlWhere += $"{property.Key}{Utils.Utils.OperatorToSqlString(op)}{(propType == typeof(string) ? $"'{val}'" : $"{val}")}";
+
+            i++;
+        }
+
+        sqlWhere += ")";
+
+        _where += _where.NullOrEmpty() ? sqlWhere : $"{(AndNodeExternal == LogicalNode.True ? " AND " : " OR ")} {sqlWhere}";
         return this;
+
+    }
+
+    public virtual GenericHandler<T> WhereNot(object entity, LogicalNode AndNodeInternal = LogicalNode.True, LogicalNode AndNodeExternal = LogicalNode.True)
+    {
+        return Where(entity, Operator.NotEquals, AndNodeInternal, AndNodeExternal);
+    }
+
+    public virtual GenericHandler<T> Where(object entity, LogicalNode AndNodeInternal = LogicalNode.True, LogicalNode AndNodeExternal = LogicalNode.True)
+    {
+        return Where(entity, Operator.Equals, AndNodeInternal, AndNodeExternal);
     }
 
     public virtual GenericHandler<T> ExplicitWhere(string explicitWhere)
